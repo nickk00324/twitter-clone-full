9edc46cd37619499965242be8600c44c3f453593
nickk <nickkochornswasdi@gmail.com>
2019-11-18 16:21:37 -0800

    production ready

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
@@ -17,6 +18,13 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
